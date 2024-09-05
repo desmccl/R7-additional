884c2d0e7920723f9bd7984101cb045128374132
Desiree Mcclain <desireemcclain51@gmail.com>
2024-09-05 13:46:31 -0400

    lesson 11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :set_common_data
+
+  private
+
+  def set_common_data
+    @customers = Customer.all
+    @orders = Order.all
+  end
 end
